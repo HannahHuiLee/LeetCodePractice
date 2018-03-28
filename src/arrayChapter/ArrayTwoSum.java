c5c6f2b8fc58af543537307b37dd638857121d1e
show111 <1793635590@qq.com>
2018-03-28 23:14:26 +0800

    finish the question on my own

--- src/arrayChapter/ArrayTwoSum.java
+++ src/arrayChapter/ArrayTwoSum.java
@@ -5,6 +5,28 @@ package arrayChapter;
  */
 public class ArrayTwoSum {
 
-    
+
+    public static void main(String[] args) {
+        int nums[] = {19, 7, 9, 24, 12, 5};
+        int target = 12;
+        int[] ints = twoSum(nums, target);
+        System.out.println("index0 is " + ints[0] + " index1 is " + ints[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] indexs = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= target) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        indexs[0] = i;
+                        indexs[1] = j;
+                    }
+                }
+            }
+        }
+        return indexs;
+    }
+
 
 }
