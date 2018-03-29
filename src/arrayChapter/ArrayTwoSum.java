759fe7323abfd135d82e1359765907d3a9c223b5
show111 <1793635590@qq.com>
2018-03-29 08:39:42 +0800

    fix two minus add

--- src/arrayChapter/ArrayTwoSum.java
+++ src/arrayChapter/ArrayTwoSum.java
@@ -7,8 +7,9 @@ public class ArrayTwoSum {
 
 
     public static void main(String[] args) {
-        int nums[] = {19, 7, 9, 24, 12, 5};
-        int target = 12;
+        int nums[] = {-1,-2,-3,-4,-5};
+        int target = -8;
+
         int[] ints = twoSum(nums, target);
         System.out.println("index0 is " + ints[0] + " index1 is " + ints[1]);
     }
@@ -16,12 +17,10 @@ public class ArrayTwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] indexs = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= target) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        indexs[0] = i;
-                        indexs[1] = j;
-                    }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    indexs[0] = i;
+                    indexs[1] = j;
                 }
             }
         }
