a882de4dfaedad19e5eb0c4099c5ee90a797508a
show111 <1793635590@qq.com>
2018-03-29 08:47:52 +0800

    the second exercise

---
+++ src/arrayChapter/ArrayRemoveDuplicates.java
@@ -0,0 +1,19 @@
+package arrayChapter;
+
+/**
+ * Created by huilee on 2018/3/29.
+ */
+public class ArrayRemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+    }
+
+    public int removeDuplicates(int[] nums) {
+
+
+        return 0;
+    }
+
+
+}
