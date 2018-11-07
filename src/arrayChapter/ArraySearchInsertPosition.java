dd12103234a071ebd601f8c3c6fdc0d577131228
huilee <huilee9999@gmail.com>
2018-11-07 08:33:38 -0500

    change

--- src/arrayChapter/ArraySearchInsertPosition.java
+++ src/arrayChapter/ArraySearchInsertPosition.java
@@ -5,6 +5,9 @@ import java.util.Vector;
 public class ArraySearchInsertPosition {
 
 
+    public static void main(String [] args){
+
+    }
 
 
     public int searchInsert(Vector<Integer> nums, int target) {
