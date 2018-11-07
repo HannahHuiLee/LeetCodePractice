df02905cf2126fc1800f03b2ac3bb91ec21bdcce
hui lee <Huilee@hfdeMacBook-Pro.local>
2018-11-07 08:07:50 -0500

    seach insert position

---
+++ src/arrayChapter/ArraySearchInsertPosition.java
@@ -0,0 +1,15 @@
+package arrayChapter;
+
+import java.util.Vector;
+
+public class ArraySearchInsertPosition {
+
+
+
+
+    public int searchInsert(Vector<Integer> nums, int target) {
+
+        return 0;
+    }
+
+}
