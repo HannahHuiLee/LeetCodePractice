4b459cb76900ffe571e79a018850782085c5c22c
huilee <huilee9999@gmail.com>
2018-11-08 07:43:16 -0500

    seach index position

---
+++ src/arrayChapter/ArraySearchIndexPosition.java
@@ -0,0 +1,26 @@
+package arrayChapter;
+
+/*
+ * created by Hannah Li on 18/11/7
+ */
+public class ArraySearchIndexPosition {
+
+    static int[] nums = {1,2,5,8};
+    static int target = 3;
+
+    public static void main(String[] args) {
+        int index = searchInsert(nums, target);
+        System.out.print(index);
+    }
+
+
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+}
