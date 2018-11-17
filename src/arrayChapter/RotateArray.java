6713abf19a1b4622a3de4f08dd08a490692531ef
huilee <huilee9999@gmail.com>
2018-11-16 23:50:34 -0500

    rotate array

---
+++ src/arrayChapter/RotateArray.java
@@ -0,0 +1,31 @@
+package arrayChapter;
+
+/**
+ * created by Hannah Li on 18/11/16
+ * Project name: LeetcodeProject
+ * LeetCode NO.: 189
+ */
+public class RotateArray {
+
+    public static void main(String[] args){
+
+    }
+
+
+    /**
+     *
+     * Input: [1,2,3,4,5,6,7] and k = 3
+     * Output: [5,6,7,1,2,3,4]
+     * Explanation:
+     * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+     * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+     * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+     *
+     */
+    public void rotate(int[] nums, int k) {
+
+
+
+    }
+
+}
