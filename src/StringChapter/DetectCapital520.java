fb43533dd3b5e25f1309fe1c6810d52693592ad7
huilee <huilee9999@gmail.com>
2019-01-31 22:53:45 -0500

    detect capital

--- src/StringChapter/DetectCapital520.java
+++ src/StringChapter/DetectCapital520.java
@@ -3,7 +3,35 @@ package StringChapter;
 /**
  * created by Hannah Li on 19/1/31
  * Project name: LeetcodeProject
- * LeetCode NO.:
+ * LeetCode NO.: 520
  */
 public class DetectCapital520 {
+
+
+    public static void main(String[] args) {
+        String s = "FlaG";
+        System.out.print(detectCapitalUse(s));
+    }
+
+    /**
+     *Given a word, you need to judge whether the usage of capitals in it is right or not.
+     * Input: "USA" ---》 True
+     * Input: "FlaG" --》 False
+     *
+     */
+    public static boolean detectCapitalUse(String word) {
+        if(word == null || word.length()<2) return true;
+
+        //All upper case && All lower case
+        if(word.toUpperCase().equals(word) || word.toLowerCase().equals(word)){
+            return true;
+        }
+
+        //Only first letter capital
+        if(Character.isUpperCase(word.charAt(0)) && word.substring(1).toLowerCase().equals(word.substring(1))){
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/StringChapter/RepeatedSubstringPattern459.java
+++ src/StringChapter/RepeatedSubstringPattern459.java
@@ -3,7 +3,35 @@ package StringChapter;
 /**
  * created by Hannah Li on 19/1/30
  * Project name: LeetcodeProject
- * LeetCode NO.:
+ * LeetCode NO.: 459
  */
 public class RepeatedSubstringPattern459 {
+
+
+    public static void main(String[] args) {
+        String s = "aabaaba";
+        System.out.print(repeatedSubstringPattern2(s));
+    }
+
+    /**
+     * Given a non-empty string check if it can be constructed by
+     * taking a substring of it and appending multiple copies of the substring together.
+     * Input: "abab" -->  True
+     */
+    public static boolean repeatedSubstringPattern2(String s) {
+        int l = s.length();
+        for(int i=l/2;i>=1;i--) {
+            if(l%i==0) {
+                int m = l/i;
+                String subS = s.substring(0,i);
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;j<m;j++) {
+                    sb.append(subS);
+                }
+                if(sb.toString().equals(s)) return true;
+            }
+        }
+        return false;
+    }
+
 }
