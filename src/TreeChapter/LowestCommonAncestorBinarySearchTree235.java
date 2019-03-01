b7c7815711b2e6463d44a36f1ab94fbb2dd47b0e
huilee <huilee9999@gmail.com>
2019-02-28 23:30:04 -0500

    lowest

---
+++ src/TreeChapter/LowestCommonAncestorBinarySearchTree235.java
@@ -0,0 +1,27 @@
+package TreeChapter;
+
+/**
+ * created by Hannah Li on 19/2/28
+ * Project name: LeetcodeProject
+ * LeetCode NO.: 235
+ */
+public class LowestCommonAncestorBinarySearchTree235 {
+
+
+    /**
+     * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+     * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+     * Output: 6
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < p.val){
+            return lowestCommonAncestor(root.right,p, q);
+        }else{
+            return root;
+        }
+    }
+
+
+}
