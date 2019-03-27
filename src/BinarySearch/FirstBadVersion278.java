398869f5d9ad6ae4a3912fb50027066699bc7472
huilee <huilee9999@gmail.com>
2019-03-26 22:54:08 -0400

    bad version2

--- src/BinarySearch/FirstBadVersion278.java
+++ src/BinarySearch/FirstBadVersion278.java
@@ -21,13 +21,17 @@ public class FirstBadVersion278 {
         int left = 1, right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
-//            if (isBadVersion(mid)) {
-//                right = mid;
-//            } else {
-//                left = mid + 1;
-//            }
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
         return left;
+    }
+
+    public boolean isBadVersion(int mid) {
+        return true;
     }
 
 }
