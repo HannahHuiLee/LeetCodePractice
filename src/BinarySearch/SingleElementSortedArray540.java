047ff60d8e861f4bd8c9cbb2e979629e6a64568c
huilee <huilee9999@gmail.com>
2020-10-15 23:11:27 -0400

    single element sorted arrays

--- src/BinarySearch/SingleElementSortedArray540.java
+++ src/BinarySearch/SingleElementSortedArray540.java
@@ -7,5 +7,28 @@ package BinarySearch;
  */
 public class SingleElementSortedArray540 {
 
+    public static void main(String[] args) {
+        SingleElementSortedArray540 sortedArray540 = new SingleElementSortedArray540();
+        int [] nums = new int[]{1,1,2,3,3,4,4,8,8};
+        System.out.println(sortedArray540.singleNonDuplicate(nums));
+    }
 
+    /**
+     * Input: nums = [1,1,2,3,3,4,4,8,8]
+     * Output: 2
+     * 一个有序数组只有一个数不出现两次，找出这个数。
+     */
+    public int singleNonDuplicate(int[] nums) {
+        int l= 0, h = nums.length - 1;
+        while (l < h){
+            int mid = l + (h - l)/2;
+            if(mid % 2 == 1) mid--;
+            if(nums[mid]==nums[mid+1]){
+                l = mid +2;
+            }else{
+                h = mid;
+            }
+        }
+        return nums[l];
+    }
 }
